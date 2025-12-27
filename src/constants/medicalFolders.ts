98154dd6af9fb770835fa0925023e2a9f76009fa
Siddy096 <varhadisiddhesh8@gmail.com>
2025-12-27 16:31:17 +0530

    Vault Integration v8

---
+++ src/constants/medicalFolders.ts
@@ -0,0 +1,8 @@
+export const MEDICAL_FOLDERS = {
+  reports: 'reports',
+  prescriptions: 'prescriptions',
+  bills: 'bills',
+  insurance: 'insurance',
+} as const;
+
+export type MedicalFolder = keyof typeof MEDICAL_FOLDERS;
