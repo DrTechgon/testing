bf4ccfa0cab40a28c19c366135431bd0be5ff38a
VedantDhoke11 <vedantdhoke11@gmail.com>
2026-02-05 13:04:12 +0530

    ngrok link added to app_api.py

--- backend/app_api.py
+++ backend/app_api.py
@@ -32,6 +32,7 @@ CORS(app, resources={
             "http://127.0.0.1:3000",
             "https://vytara-official.vercel.app",
             "https://*.vercel.app"
+            "https://sauncier-instigative-yolande.ngrok-free.dev"
         ],
         "methods": ["GET", "POST", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
